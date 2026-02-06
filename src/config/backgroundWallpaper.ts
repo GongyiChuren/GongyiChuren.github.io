38315d2b1f7d59ce10606b32a2268c52baf84ab1
InfernoXuaI <1391197588@qq.com>
2026-02-07 00:57:30 +0800

    2.7

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -90,13 +90,13 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			},
 		},
 		// 横幅导航栏配置
-		navbar: {
+			navbar: {
 			// 横幅导航栏透明模式："semi" 半透明，"full" 完全透明，"semifull" 动态透明
 			transparentMode: "semifull",
 			// 是否开启毛玻璃模糊效果，开启可能会影响页面性能，如果不开启则是半透明，请根据自己的喜好开启
 			enableBlur: true,
 			// 毛玻璃模糊度
-			blur: 3,
+			blur: 2,
 		},
 		// 波浪动画效果配置，开启可能会影响页面性能，请根据自己的喜好开启
 		waves: {
@@ -113,7 +113,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 层级，确保壁纸在背景层
 		zIndex: -1,
 		// 壁纸透明度
-		opacity: 0.8,
+		opacity: 0.72,
 		// 背景模糊程度
 		blur: 1,
 	},
